039667f04692545161be0b1d50ec171709794810
Emma Koslow <emmakoslowacea@gmail.com>
2020-07-20 12:46:18 -0400

    Updates

---
+++ precache-manifest.12d7b072d190056d82085bc360d4b005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eda7594f5e0dea5c10f604978b9fe20",
+    "url": "/censor-app/index.html"
+  },
+  {
+    "revision": "3f9fffd09bfe36044d27",
+    "url": "/censor-app/static/css/main.dba0ff61.chunk.css"
+  },
+  {
+    "revision": "43fdd6f19f8b4fb7528e",
+    "url": "/censor-app/static/js/2.8081fe46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/censor-app/static/js/2.8081fe46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9fffd09bfe36044d27",
+    "url": "/censor-app/static/js/main.2d02691f.chunk.js"
+  },
+  {
+    "revision": "12a389e548a9e6afa565",
+    "url": "/censor-app/static/js/runtime-main.2f086fe4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/censor-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
